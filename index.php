c4c14178503775b2d5a3a7637a2d7965002485d0
Marlon McPherson <mmcpherson5@cnm.edu>
2018-05-11 09:02:48 -0600

    completed challenge

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>snap-fetch</title>
+	<script type="text/javascript" src="script.js"></script>
+
+</head>
+	<body>
+
+	<p></p>
+	</body>
+</html>
